18ea65b27e81cd7fd09fd6d4bdf2d439523f4a9b
menkooky <dawahmenna@std.mans.edu.eg>
2024-02-23 19:36:27 +0200

    2/23/2024

--- lib/features/complete_profile_screen/user_profile/screen3.dart
+++ lib/features/complete_profile_screen/user_profile/screen3.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:mediator/widgets/app_button.dart';
-import 'package:phosphor_flutter/phosphor_flutter.dart';
 
 import '../../../core/app_colors.dart';
 
