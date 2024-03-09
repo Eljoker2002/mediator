f493f3657509cfdd53a3236d3d0cdddd34a6b12f
Eljoker2002 <se.ahmedfahmy@gmail.com>
2024-03-09 15:57:54 +0200

    ...

--- lib/features/status/status.dart
+++ lib/features/status/status.dart
@@ -22,216 +22,236 @@ class _StatusState extends State<Status> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding:EdgeInsets.only(top: 55.h,left: 20.w),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                Icon(Icons.arrow_back_ios),
-                SizedBox(width: 110.w),
-                Text(
-                  textAlign: TextAlign.center,
-                  "Status",
-                  style: GoogleFonts.inter(
-                    textStyle: TextStyle(
-                      fontSize: 18.sp,
-                      fontWeight: FontWeight.w700,
-                      color: AppColors.blue0C,
-                    ),
+        padding: EdgeInsets.only(top: 55.h, left: 20.w),
+        child: Column(children: [
+          Row(
+            children: [
+              Icon(Icons.arrow_back_ios),
+              SizedBox(width: 110.w),
+              Text(
+                textAlign: TextAlign.center,
+                "Status",
+                style: GoogleFonts.inter(
+                  textStyle: TextStyle(
+                    fontSize: 18.sp,
+                    fontWeight: FontWeight.w700,
+                    color: AppColors.blue0C,
                   ),
                 ),
+              ),
+            ],
+          ),
+          SizedBox(height: 5.h),
+          Padding(
+            padding: EdgeInsets.only(right: 55.w),
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isChecked1 = !_isChecked1;
+                    });
+                  },
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
+                      ),
+                      color: _isChecked1 ? Colors.blue : Colors.transparent,
+                    ),
+                    child: _isChecked1
+                        ? Icon(
+                            Icons.check,
+                            size: 18.0,
+                            color: Colors.white,
+                          )
+                        : null,
+                  ),
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                RefactorCheckboxText(
+                    text1: "Looking for a jobs",
+                    text2: '''I am looking for a job and i would like to 
+accept job invitations.'''),
               ],
             ),
-            SizedBox(height: 5.h),
-            Padding(
-        padding:EdgeInsets.only(right:56.w),
-        child: Row(
-          children: [
-            InkWell(
-                onTap: () {
-                  setState(() {
-                    _isChecked1 = !_isChecked1;
-                  });
-                },
-                child: Container(
-                  width: 20.0,
-                  height: 20.0,
-                  decoration: BoxDecoration(
-                    shape: BoxShape.circle,
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 2.0,
-                    ),
-                    color: _isChecked1 ? Colors.blue : Colors.transparent,
-                  ),
-                  child: _isChecked1
-                      ? Icon(
-                    Icons.check,
-                    size: 18.0,
-                    color: Colors.white,
-                  )
-                      : null,
-                ),
-            ),
-            SizedBox(width: 15.w,),
-            RefactorCheckboxText(text1:"Looking for a jobs" , text2: '''I am looking for a job and i would like to 
-accept job invitations.'''),
-          ],
-        ),
-      ),
-            SizedBox(height: 6.h),
-            Padding(
-              padding:EdgeInsets.only(right:56.w),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        _isChecked2 = !_isChecked2;
-                      });
-                    },
-                    child: Container(
-                      width: 20.0,
-                      height: 20.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.0,
-                        ),
-                        color: _isChecked2 ? Colors.blue : Colors.transparent,
+          ),
+          SizedBox(height: 6.h),
+          Padding(
+            padding: EdgeInsets.only(right: 55.w),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isChecked2 = !_isChecked2;
+                    });
+                  },
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
                       ),
-                      child: _isChecked2
-                          ? Icon(
-                        Icons.check,
-                        size: 18.0,
-                        color: Colors.white,
-                      )
-                          : null,
+                      color: _isChecked2 ? Colors.blue : Colors.transparent,
                     ),
+                    child: _isChecked2
+                        ? Icon(
+                            Icons.check,
+                            size: 18.0,
+                            color: Colors.white,
+                          )
+                        : null,
                   ),
-                  SizedBox(width: 15.w,),
-                  RefactorCheckboxText(text1:"Looking for an internships " , text2:'''I am looking for an internship and
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                RefactorCheckboxText(
+                    text1: "Looking for an internships ",
+                    text2: '''I am looking for an internship and
 i would like to accept internship
 invitations.'''),
-                ],
-              ),
+              ],
             ),
-            SizedBox(height: 6.h),
-            Padding(
-              padding:EdgeInsets.only(right:56.w),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        _isChecked3 = !_isChecked3;
-                      });
-                    },
-                    child: Container(
-                      width: 20.0,
-                      height: 20.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.0,
-                        ),
-                        color: _isChecked3 ? Colors.blue : Colors.transparent,
+          ),
+          SizedBox(height: 6.h),
+          Padding(
+            padding: EdgeInsets.only(right: 55.w),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isChecked3 = !_isChecked3;
+                    });
+                  },
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
                       ),
-                      child: _isChecked3
-                          ? Icon(
-                        Icons.check,
-                        size: 18.0,
-                        color: Colors.white,
-                      )
-                          : null,
+                      color: _isChecked3 ? Colors.blue : Colors.transparent,
                     ),
+                    child: _isChecked3
+                        ? Icon(
+                            Icons.check,
+                            size: 18.0,
+                            color: Colors.white,
+                          )
+                        : null,
                   ),
-                  SizedBox(width: 15.w,),
-                  RefactorCheckboxText(text1:"Looking for competitions" , text2:'''I am looking for a competition and i 
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                RefactorCheckboxText(
+                    text1: "Looking for competitions",
+                    text2: '''I am looking for a competition and i 
 would like to accept competition 
 invitations.'''),
-                ],
-              ),
+              ],
             ),
-            SizedBox(height: 6.h),
-            Padding(
-              padding:EdgeInsets.only(right:56.w),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        _isChecked4 = !_isChecked4;
-                      });
-                    },
-                    child: Container(
-                      width: 20.0,
-                      height: 20.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.0,
-                        ),
-                        color: _isChecked4 ? Colors.blue : Colors.transparent,
+          ),
+          SizedBox(height: 6.h),
+          Padding(
+            padding: EdgeInsets.only(right: 55.w),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isChecked4 = !_isChecked4;
+                    });
+                  },
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
                       ),
-                      child: _isChecked4
-                          ? Icon(
-                        Icons.check,
-                        size: 18.0,
-                        color: Colors.white,
-                      )
-                          : null,
+                      color: _isChecked4 ? Colors.blue : Colors.transparent,
                     ),
+                    child: _isChecked4
+                        ? Icon(
+                            Icons.check,
+                            size: 18.0,
+                            color: Colors.white,
+                          )
+                        : null,
                   ),
-                  SizedBox(width: 15.w,),
-                  RefactorCheckboxText(text1:"Looking for Tasks" , text2:'''I am looking for an employees and 
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                RefactorCheckboxText(
+                    text1: "Looking for Tasks",
+                    text2: '''I am looking for an employees and 
 i would like to see employees applying 
 for this position.'''),
-                ],
-              ),
+              ],
             ),
-            SizedBox(height: 6.h),
-            Padding(
-              padding:EdgeInsets.only(right:56.w),
-              child: Row(
-                children: [
-                  InkWell(
-                    onTap: () {
-                      setState(() {
-                        _isChecked5 = !_isChecked5;
-                      });
-                    },
-                    child: Container(
-                      width: 20.0,
-                      height: 20.0,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        border: Border.all(
-                          color: Colors.grey,
-                          width: 2.0,
-                        ),
-                        color: _isChecked5 ? Colors.blue : Colors.transparent,
+          ),
+          SizedBox(height: 6.h),
+          Padding(
+            padding: EdgeInsets.only(right: 55.w),
+            child: Row(
+              children: [
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      _isChecked5 = !_isChecked5;
+                    });
+                  },
+                  child: Container(
+                    width: 20.0,
+                    height: 20.0,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(
+                        color: Colors.grey,
+                        width: 2.0,
                       ),
-                      child: _isChecked5
-                          ? Icon(
-                        Icons.check,
-                        size: 18.0,
-                        color: Colors.white,
-                      )
-                          : null,
+                      color: _isChecked5 ? Colors.blue : Colors.transparent,
                     ),
+                    child: _isChecked5
+                        ? Icon(
+                            Icons.check,
+                            size: 18.0,
+                            color: Colors.white,
+                          )
+                        : null,
                   ),
-                  SizedBox(width: 15.w,),
-                  RefactorCheckboxText(text1:"Looking for Courses & Roadmaps" , text2:'''I am looking for a courses and 
+                ),
+                SizedBox(
+                  width: 15.w,
+                ),
+                RefactorCheckboxText(
+                    text1: "Looking for Courses & Roadmaps",
+                    text2: '''I am looking for a courses and 
 roadmaps and i would like to see 
 employees applying for this position.'''),
-                ],
-              ),
+              ],
             ),
-          ]),
+          ),
+        ]),
       ),
     );
   }
@@ -240,7 +260,7 @@ employees applying for this position.'''),
 class RefactorCheckboxText extends StatelessWidget {
   final String text1;
   final String text2;
- 
+
   const RefactorCheckboxText({
     Key? key,
     required this.text1,
@@ -250,7 +270,7 @@ class RefactorCheckboxText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding:EdgeInsets.only(top: 28.h),
+      padding: EdgeInsets.only(top: 28.h),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -279,4 +299,3 @@ class RefactorCheckboxText extends StatelessWidget {
     );
   }
 }
-
